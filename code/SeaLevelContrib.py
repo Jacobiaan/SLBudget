ab961ba7237584a476fce64ce9b30746a1611a70
dlebars <dewi.lebars@gmail.com>
2020-04-09 18:02:02 +0200

    Continued fixing some paths changes and restructuring Steric.ipynb. Ready to include new steric data from Chen

--- code/SeaLevelContrib.py
+++ code/SeaLevelContrib.py
@@ -221,7 +221,9 @@ def tide_gauge_obs(tg_id=[20, 22, 23, 24, 25, 32], interp=False):
 def StericSL_EN4(max_depth, mask_name):
     '''Compute the steric effect in the North Sea in cm integrated from the 
     surface up to a given depth given in meters. '''
-    DENS = xr.open_dataset('density_teos10_en4_1900_2019.nc')
+    DENS = xr.open_dataset(PATH_SLBudgets_data + 
+                           'DataDensity/density_teos10_EN421f_analysis_g10/' + 
+                           'density_teos10_en4_1900_2019.nc')
     midp = (np.array(DENS.depth[1:])+np.array(DENS.depth[:-1]))/2
     midp = np.insert(midp, 0, np.array([0]))
     midp = np.insert(midp, len(midp), np.array(DENS.depth[-1]) + 
